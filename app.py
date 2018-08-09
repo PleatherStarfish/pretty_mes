5f3f1d1e23498e1843816894ef2a37f6c43ea4b9
Daniel Miller <lontano.composer@gmail.com>
2018-08-09 14:52:18 -0400

    first

--- app.py
+++ app.py
@@ -147,5 +147,5 @@ def index():
     return render_template('index.html', poem = poem_list)
 
 if __name__ == '__main__':
-    # port = int(os.environ.get('PORT', 5000))
-    app.run(debug = True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug = True, port=port)
